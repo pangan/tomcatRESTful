e01e0d640dd1a2d9522c90d1c00ac36cb63ad160
pangan <pangan@gmail.com>
2017-03-09 14:33:05 +0100

    added logger

--- src/main/java/com/mofakhar/info/server/Server.java
+++ src/main/java/com/mofakhar/info/server/Server.java
@@ -1,5 +1,7 @@
 package com.mofakhar.info.server;
 
+import javassist.bytecode.stackmap.TypeData;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -9,6 +11,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 
 /**
  * Created by pangan on 08/03/2017.
@@ -16,9 +23,18 @@ import java.io.InputStreamReader;
 @Path("/hello")
 public class Server {
 
+    private static final Logger LOGGER = Logger.getLogger( TypeData.ClassName.class.getName());
+
+    public Server() throws IOException {
+        Handler fh = new FileHandler("%t/tomcatrest.log");
+        fh.setFormatter(new SimpleFormatter());
+        LOGGER.getLogger("").addHandler(fh);
+        LOGGER.info("start");
+    }
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayPlainTextHello() {
+    public String sayPlainTextHello(){
+        LOGGER.info("GET method received.");
         return "Hello GET method!";
     }
 
@@ -26,7 +42,8 @@ public class Server {
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPutHello(InputStream data){
 
-        
+
+
         String line = "";
         StringBuilder sb = new  StringBuilder();
         try {
@@ -38,7 +55,7 @@ public class Server {
             e.printStackTrace();
         }
         String ret_value = String.format("Received data: %s \n", sb.toString());
-
+        LOGGER.info(String.format("PUT method received : %s", sb.toString()));
         return ret_value;
     }
 }
