4fa319314917d66da0358afc99eb187a00ba0dad
pangan <pangan@gmail.com>
2017-03-09 09:36:27 +0100

    Receive data

--- src/main/java/com/mofakhar/info/server/Server.java
+++ src/main/java/com/mofakhar/info/server/Server.java
@@ -5,6 +5,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by pangan on 08/03/2017.
@@ -12,7 +16,6 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class Server {
 
-    // This method is called if TEXT_PLAIN is request
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPlainTextHello() {
@@ -21,7 +24,21 @@ public class Server {
 
     @PUT
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayPutHello(){
-        return "Hello PUT method!";
+    public String sayPutHello(InputStream data){
+
+        
+        String line = "";
+        StringBuilder sb = new  StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(data));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String ret_value = String.format("Received data: %s \n", sb.toString());
+
+        return ret_value;
     }
 }
